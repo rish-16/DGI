567628a3e4d29ea877e0d25bd30b13e2d6080c5f
Rishabh Anand <mail.rishabh.anand@gmail.com>
2022-08-28 19:40:58 +0800

    Add results

---
+++ analysis.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import json, os
+
+PATH = "stats/"
+
+accs = []
+rates = []
+
+for fle in os.listdir(PATH):
+    path_string = PATH + fle
+    with open(path_string, "r") as f:
+        data = json.load(f)
+
+    accs.append(data['acc_mean'])
+    rates.append(data['damage_rate'])
+
+assert len(accs) == len(rates)
+new_data = [[accs[i], rates[i]] for i in range(len(accs))]
+new_data.sort(key=lambda r : r[1])
+accs = [rec[0] for rec in new_data]
+rates = [rec[1] for rec in new_data]
+
+plt.plot(rates, accs, color="green", marker="*", markersize=7)
+plt.xlabel("Masked node feature rate [0, 1.0]")
+plt.ylabel("Test accuracy %")
+plt.title("DGI on missing node features")
+plt.show()
